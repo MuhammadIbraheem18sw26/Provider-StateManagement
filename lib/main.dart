b36678d236e17e0be3b1a6e9e23f8dd240f86a6a
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-06-25 18:53:03 +0500

    adding ChangeNotiferProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
+import './data/repository.dart';
+import 'package:provider/provider.dart';
 
+import 'data/memory_repository.dart';
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
@@ -23,16 +26,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Recipes_Finder',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.white,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider<MemoryRepository>(
+      lazy: false,
+      create: (_) => MemoryRepository(),
+      child: MaterialApp(
+        title: 'Recipes_Finder',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.white,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const MainScreen(),
       ),
-      home: const MainScreen(),
     );
   }
 }
